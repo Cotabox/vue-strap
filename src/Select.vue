e8071ab14d1f0c884f61432aed19896c21df9c5b
João Cavalcante <contato@kavalcante.com>
2017-04-12 17:49:54 -0300

    New Select.vue

--- src/Select.vue
+++ src/Select.vue
@@ -294,7 +294,7 @@ export default {
 .bs-searchbox input:focus,
 .form-control.dropdown-toggle:focus {
   outline: 0;
-  border-color: #66afe9 !important;
+  border-color: #66afe9;
   box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);
 }
 .secret {
